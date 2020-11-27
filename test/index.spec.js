a38580e0d52797dc11bd56910364369b208f9f25
Md Irshad <mdirshaddev@gmail.com>
2020-11-27 20:53:10 +0530

    Trying to connect travis with heroku.

---
+++ test/index.spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect
+const server = require('../main');
+
+describe('test', () => {
+  it('should return the sum', () => {
+    expect(2+2).to.equal(4);
+  });
+});
